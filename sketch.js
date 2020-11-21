c789ef9a6c541b0ca1798c357ef0752117b3c32e
Narain Bajaj <74300346+Narain-18@users.noreply.github.com>
2020-11-21 15:13:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var movingRect, fixedRect;
+
+function setup() {
+  createCanvas(800,400);
+  movingRect = createSprite(400, 200, 50, 50);
+  movingRect.shapeColor="cyan";
+  fixedRect = createSprite(200,200,60,70);
+  fixedRect.shapeColor="orange";
+  
+}
+
+function draw() {
+  background(0);  
+  
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+
+  // m-f = 5+3 or f-m = 3+5
+  if(movingRect.x - fixedRect.x < movingRect.width/2 + fixedRect.width/2 &&
+     fixedRect.x - movingRect.x < movingRect.width/2 + fixedRect.width/2 &&
+     movingRect.y - fixedRect.y < movingRect.height/2 + fixedRect.height/2 &&
+     fixedRect.y - movingRect.y < movingRect.height/2 + fixedRect.height/2 ){
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor ="red";
+  }
+
+  else{
+    movingRect.shapeColor = "cyan";
+    fixedRect.shapeColor = "orange";
+  }
+  drawSprites();
+}
